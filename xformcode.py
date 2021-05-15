e30ee9ec51ad306ec7b5a04443a36ed1c9456d1d
Jagane Sundar <jagane@infinstor.com>
2021-05-14 17:03:32 -0700

    service name is now passed in as a param

--- xformcode.py
+++ xformcode.py
@@ -4,12 +4,18 @@ import subprocess
 import time
 
 token = None
+service = None
 for x in range(1, len(sys.argv)):
     if (sys.argv[x].startswith('--token')):
         token = sys.argv[x][8:]
+    elif (sys.argv[x].startswith('--service')):
+        service = sys.argv[x][10:]
 
 if (not token):
-    raise Exception('insufficient parameters')
+    raise Exception('insufficient parameters: token necessary')
+
+if (not service):
+    raise Exception('insufficient parameters: service name necessary')
 
 cognito_username=os.getenv('COGNITO_USERNAME')
 if (not cognito_username):
@@ -33,8 +39,8 @@ subprocess.run(['/opt/conda/bin/jupyter-labhub', '--debug', '-y', '--no-browser'
         JUPYTERHUB_OAUTH_CALLBACK_URL='/user/' + cognito_username + '/oauth_callback',
         JUPYTERHUB_USER=cognito_username,
         JUPYTERHUB_SERVER_NAME='',
-        JUPYTERHUB_API_URL='https://jupyterhub.infinstor.com:444/hub/api',
-        JUPYTERHUB_ACTIVITY_URL='https://jupyterhub.infinstor.com:444/hub/api/users/' + cognito_username + '/activity',
+        JUPYTERHUB_API_URL='https://jupyterhub.' + service + ':444/hub/api',
+        JUPYTERHUB_ACTIVITY_URL='https://jupyterhub.' + service + ':444/hub/api/users/' + cognito_username + '/activity',
         JUPYTERHUB_ACTIVITY_INTERVAL='60',
         JUPYTERHUB_BASE_URL='/',
         JUPYTERHUB_SERVICE_PREFIX='/user/' + cognito_username + '/'))
